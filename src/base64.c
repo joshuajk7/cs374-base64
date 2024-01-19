f70a246078658687f0bf7940ed3134074bb1df60
Joshua Kim <joshuajk7@gmail.com>
2024-01-18 17:07:07 -0800

    finished src/base64.c assignment

--- src/base64.c
+++ src/base64.c
@@ -5,6 +5,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+
+# if CHAR_BIT != 8
+# error "CHAR_BIT must be 8!"
+#endif
 
 #include "gprintf.h"
 
@@ -16,20 +21,67 @@ static char const b64a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 int
 main(int argc, char *argv[])
 {
-  printf("The base64 alphabet is: %s\n", b64a);
+  char const *fn = "-";
 
-  /* You can use the gprintf function provided along with this source code to
-   * print debugging messages, which won't show up in your release build.
-   */
-  gprintf("I received %d arguments\n", argc);
-  for (int i = 0; i < argc; ++i) {
-    gprintf("Argument %d is %s\n", i, argv[i]);
+  if (argc > 2) {
+    errx(1, "Usage: %s [FILE]", argv[0]);
+  } else if (argc > 1) {
+      fn = argv[1]; 
+      void *res = freopen(fn, "r", stdin);
+      if (!res) err(1, "%s", fn);
+    }
+
+
+  int line_char = 0;
+  for (;;) {
+    unsigned char buf[3] = {0};
+
+    size_t nr = fread(buf, 1, sizeof buf, stdin);
+    if (nr < sizeof buf && ferror(stdin)) {
+        err(EXIT_FAILURE, "%s", fn);
+      }
+      if (nr == 0) break;
+
+      long long unsigned int bits = 0;
+      bits |= buf[0];
+      bits <<= CHAR_BIT;
+      bits |= buf[1];
+      bits <<= CHAR_BIT;
+      bits |= buf[2];
+
+      unsigned char b64i[4];
+      b64i[3] = bits & 0x3f;
+      bits >>= 6;
+      b64i[2] = bits & 0x3f;
+      bits >>= 6; 
+      b64i[1] = bits & 0x3f;
+      bits >>= 6;
+      b64i[0] = bits;
+
+      char b64o[4];
+      b64o[0] = b64a[b64i[0]];
+      b64o[1] = b64a[b64i[1]];
+      b64o[2] = b64a[b64i[2]];
+      b64o[3] = b64a[b64i[3]];
+
+      if (nr < 3) b64o[3] = '=';
+      if (nr < 2) b64o[2] = '=';
+
+      fwrite(b64o, 1, sizeof b64o, stdout);
+      fflush(stdout);
+      line_char += sizeof b64o; 
+
+      if (line_char >= 76) {
+        assert(line_char == 76);
+        line_char = 0;
+        putchar('\n');
+     }
+
+      if (nr < 3) break;
+    }
+
+    if (line_char != 0) putchar('\n');
+
+  return EXIT_SUCCESS;
   }
 
-  assert(0); /* Notice that assertions only affect the debug build; this is
-                because the NDEBUG macro is defined in the command-line of the
-                release build (-DNDEBUG) */
-
-  puts("Made it to the end! Goodbye :)");
-  return EXIT_SUCCESS;
-}
